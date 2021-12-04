ec5a5eef7d97d7646f431afc5b029bb5760fe9a1
blackpenguin98 <euclidesmarquez98@outlook.com>
2021-12-04 15:39:09 -0600

    Minions

---
+++ Backtrace GameJam/Assets/Scripts/Enemies/minionMain.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class minionMain : MonoBehaviour
+{
+
+    public GameObject player;
+    public float persueDistance, attackDistance;
+    Transform originalPos;
+
+    float timePassed;
+    public float waitingTime = 2;
+
+    Animator animator;
+
+    NavMeshAgent navMeshAgent;
+
+    bool returning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        originalPos = transform;
+        timePassed = Mathf.Infinity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.LookAt(player.transform);
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= persueDistance)
+        {
+            navMeshAgent.destination = player.transform.position + new Vector3(-2, 0 - 2);
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            navMeshAgent.destination = originalPos.position;
+            animator.SetBool("walking", false);
+        }
+
+
+        if(Vector3.Distance(transform.position, player.transform.position) <= attackDistance && timePassed >= waitingTime)
+        {
+            timePassed = 0;
+            animator.SetTrigger("attack");
+        }
+
+
+        timePassed += Time.deltaTime;
+
+
+
+
+           
+        }
+
+
+    }
+
+
+  
+
+
